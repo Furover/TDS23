2bdad4d3c41a99895ffe587c2d87a28a8f064138
Furover <fur.wiggywag@protonmail.com>
2023-07-16 20:04:57 -0300

    weird bug found

--- JAVA/LOJAFOMY/src/app/AppLoja.java
+++ JAVA/LOJAFOMY/src/app/AppLoja.java
@@ -5,27 +5,26 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
-import entities.ListaProduto;
+
 import entities.Produto;
 
 public class AppLoja {
 	
-	public static List <Produto> list = new ArrayList<>();
+	
+	public static Produto lp = new Produto();
 	
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
+		
+		lp.declareTheOnes();
+		
+		firstScreen();
+		
+	}
+	
+	public static void firstScreen() {
 		Scanner read = new Scanner(System.in);
 		char answer;
-		list.add(new Produto("GRP0X-01", "BONÉ             ", 100.00, 10, 0));
-		list.add(new Produto("GRP0X-02", "CALÇA             ", 150.00, 10, 0));
-		list.add(new Produto("GRP0X-03", "BERMUDA         ", 90.00, 10, 0));
-		list.add(new Produto("GRP0X-04", "BATA             ", 140.00, 10, 0));
-		list.add(new Produto("GRP0X-05", "CAMISA             ", 150.00, 10, 0));
-		list.add(new Produto("GRP0X-06", "SAIA RODADA        ", 90.00, 10, 0));
-		list.add(new Produto("GRP0X-07", "SAIA STYLE       ", 90.00, 10, 0));
-		list.add(new Produto("GRP0X-08", "MACACÃO AFRO    ", 150.00, 10, 0));
-		list.add(new Produto("GRP0X-09", "CAMISETA TEMATICA", 100.00, 10, 0));
-		list.add(new Produto("GRP0X-10", "CALÇA SOCIAL AFRO", 95.50, 10, 0));
 		Space(10);
 		System.out.print("FOMY  STORE");
 		Jump(1);
@@ -44,23 +43,16 @@ public class AppLoja {
 		}
 		else {
 			Jump(300);
-			System.out.println("BRUH, aprenda a escrever!!!");
+			System.out.println("Aprenda a escrever!!!");
 			Jump(2);
-			main(null);
+			firstScreen();
 			
 		}
-	
-	
-		
-		
-		
-
 	}
 	
 	public static void PrimaryScreen() {
-		ListaProduto lsp1 = new ListaProduto();
 		Jump(300);
-		lsp1.Lista(true);
+		lp.Lista(true);
 		Jump(1);
 		DecisionTime();
 	}
@@ -68,7 +60,7 @@ public class AppLoja {
 		Scanner read = new Scanner(System.in);
 		String answer;
 		boolean wrong = true;
-		double totalValue = 0;
+		double totalValue = 0.00;
 		AppLoja.Jump(300);
 		AppLoja.Space(30);
 		System.out.print("FOMY  STORE");
@@ -81,14 +73,14 @@ public class AppLoja {
 		System.out.println("Código\t\tProduto\t\t\tValor\tQuantidade");
 		AppLoja.Lines(75);
 		AppLoja.Jump(1);
-		for(Produto x : list) {
+		for(Produto x : lp.list) {
 			if(x.getCart() > 0) {
 				System.out.println( x.toStringCart());
 				totalValue += (x.getValue() * x.getCart());
 			}
 		}
-		System.out.printf("\n\nValor total: R$ %.2f",totalValue);
-		AppLoja.Lines(75);
+		System.out.printf("\n\nValor total: R$ %.2f\n",totalValue);
+		Lines(75);
 		Jump(1);
 		while(wrong == true) {
 			System.out.println("Deseja confirmar a compra?");
@@ -105,13 +97,16 @@ public class AppLoja {
 				Jump(1);
 			}
 		}
-		
-		System.out.printf("\n\nOPÇÕES DE PAGAMENTO\n\n1 - A VISTA COM 10% DESCONTO: %.2f",(totalValue * 0.9));
-		System.out.printf("\n2 - NO CARTÃO COM 10% DE JUROS: %.2f",(totalValue * 1.10));
+		double totalValue2 = 0.0;
+		totalValue2 = (totalValue * 0.9);
+		System.out.printf("\n\nOPÇÕES DE PAGAMENTO\n\n1 - A VISTA COM 10% DESCONTO: %f",totalValue2);
+		totalValue2 = totalValue * 1.1;
+		System.out.printf("\n2 - NO CARTÃO COM 10% DE JUROS: %.2f",totalValue2);
 		double parcela1 = ((totalValue / 2) * 1.15);
 		double parcela2 = (parcela1 * 1.15);
+		totalValue2 = parcela1 + parcela2;
 		System.out.printf("\n3 - NO CARTÃO EM DUAS VEZES COM 15% DE JUROS:");
-		System.out.printf("\nPARCELA 1: %.2f",parcela1,"\nPARCELA 2: %.2f",parcela2,"\nTOTAL: %.2f",(parcela1 + parcela2),"\n\n");
+		System.out.printf("\nPARCELA 1: %.2f",parcela1,"\nPARCELA 2: %.2f",parcela2,"\nTOTAL: %.2f",totalValue2,"\n\n");
 		System.out.println("Digite o número da opção que deseja utilizar:\n");
 		answer = read.next();
 		while(wrong == false) {
@@ -147,7 +142,7 @@ public class AppLoja {
 		answer = read.next().toUpperCase();
 		while(foundIt == false) {
 			int tries = -1;
-			for(Produto x : list) {
+			for(Produto x : lp.list) {
 				tries++;
 				if(answer.equals(x.getCode())) {
 					Jump(1);
@@ -157,7 +152,7 @@ public class AppLoja {
 					Lines(75);
 					Jump(1);
 					System.out.println("Quantos deseja desse item?");
-					qty = read.nextInt();
+					x.addCart(read.nextInt());
 					read.nextLine();
 					while(wrong == true) {
 						System.out.println("Deseja continuar comprando?");
@@ -165,13 +160,11 @@ public class AppLoja {
 						if(answer.equals("S") || answer.equals("SIM")) {
 							wrong = false;
 							foundIt = true;
-							x.setCart(qty);
 							PrimaryScreen();
 						}
 						else if(answer.equals("N") || answer.equals("NAO")){
 							wrong = false;
 							foundIt = true;
-							x.setCart(qty);
 							Cart();
 						}
 						else {
@@ -181,7 +174,7 @@ public class AppLoja {
 					}
 				
 				}
-				else if(tries == (list.size() - 1)) {
+				else if(tries == (lp.list.size() - 1)) {
 					System.out.println("Não achei, escreva novamente:");
 					answer = read.next().toUpperCase();
 					tries = -1;
--- JAVA/LOJAFOMY/src/entities/Produto.java
+++ JAVA/LOJAFOMY/src/entities/Produto.java
@@ -1,7 +1,15 @@
 package entities;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import app.AppLoja;
+
 public class Produto {
 	
+
+	public List <Produto> list = new ArrayList<>();
 	private String code;
 	private String prod;
 	private double value;
@@ -18,6 +26,65 @@ public class Produto {
 	}
 	public Produto() {
 		
+	}
+	
+	public void addCart(int qty) {
+		Scanner retry = new Scanner(System.in);
+		if(qty <= 0 || qty > this.getStock()) {
+			System.out.println("Valor inválido, digite novamente!");
+			addCart(retry.nextInt());
+		}
+		else {
+			this.cart += qty;
+		}
+	}
+	public void removeCart(int qty) {
+		Scanner retry = new Scanner(System.in);
+		if(qty <= 0 || qty > this.getStock()) {
+			System.out.println("Valor inválido, digite novamente!");
+			removeCart(retry.nextInt());
+		}
+		else {
+			this.cart -= qty;
+		}
+	}
+	public void Lista(boolean slogan) {
+		
+		if(slogan == true) {
+			AppLoja.Jump(300);
+			AppLoja.Space(30);
+			System.out.print("FOMY  STORE");
+			AppLoja.Jump(1);
+			AppLoja.Space(32);
+			System.out.print("ALBERTO");
+			AppLoja.Jump(1);
+			AppLoja.Lines(75);
+			AppLoja.Jump(2);
+			System.out.println("Código\t\tProduto\t\t\tValor\tEstoque\t\tCarrinho");
+			AppLoja.Lines(75);
+			AppLoja.Jump(1);
+			for(Produto x : list) {
+				System.out.println(x.toString());
+			}
+			AppLoja.Lines(75);
+		}
+		else {
+			for(Produto x : list) {
+				System.out.println(x.toString());
+			}
+		}
+	}
+	public void declareTheOnes() {
+		list.add(new Produto("GRP0X-01", "BONÉ             ", 100.00, 10, 0));
+		list.add(new Produto("GRP0X-02", "CALÇA             ", 150.00, 10, 0));
+		list.add(new Produto("GRP0X-03", "BERMUDA         ", 90.00, 10, 0));
+		list.add(new Produto("GRP0X-04", "BATA             ", 140.00, 10, 0));
+		list.add(new Produto("GRP0X-05", "CAMISA             ", 150.00, 10, 0));
+		list.add(new Produto("GRP0X-06", "SAIA RODADA        ", 90.00, 10, 0));
+		list.add(new Produto("GRP0X-07", "SAIA STYLE       ", 90.00, 10, 0));
+		list.add(new Produto("GRP0X-08", "MACACÃO AFRO    ", 150.00, 10, 0));
+		list.add(new Produto("GRP0X-09", "CAMISETA TEMATICA", 100.00, 10, 0));
+		list.add(new Produto("GRP0X-10", "CALÇA SOCIAL AFRO", 95.50, 10, 0));
 	}
 
 	public String getCode() {
