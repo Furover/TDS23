8cbd8c00d211bf65fda4dcbf3660a2a9e2c2a2b8
Furover <133924982+Geafy@users.noreply.github.com>
2023-07-27 00:09:27 +0000

    Add files via upload

---
+++ SQL/InsertUsuario.sql
@@ -0,0 +1,16 @@
+USE [FomyDB]
+GO
+
+INSERT INTO [dbo].[Usuario]
+           ([Email]
+           ,[Nome]
+           ,[Senha]
+           ,[IdNivel]
+           ,[Experiencia]
+           ,[Foto]
+           ,[Moedas]
+           ,[DataCadastro])
+     VALUES
+           ('pedrinho@gmail.com','pedro","pedorfutebol2010' ,0 ,0,'/photos/038295325.jpg',0,GETDATE())
+GO
+
