6484cc6d5755ed94adf639f1837876b209cb5a12
Furover <3264.343@educacao.org.br>
2023-07-31 19:07:31 -0300

    stuff

--- FOMYSQL/SelectTrilhas.sql
+++ FOMYSQL/SelectTrilhas.sql
@@ -1,3 +1,5 @@
-select  t.IdTrilha , nomeTrilha, t.Descricao  from ReceitaTrilha rt
+select  t.IdTrilha , nomeTrilha, t.Descricao, ur.IdUsuRece  from ReceitaTrilha rt
 inner join Trilhas t on t.IdTrilha = rt.IdTrilha 
 inner join Receitas r  on r.IdReceita  = rt.IdReceita
+INNER JOIN UsuarioReceita ur ON ur.IdTrilha = r.IdReceita
+INNER JOIN Usuario u ON u.IdUsuario = ur.IdUsuario 
