5c8f162f78bb673bf1cc6f9809fedef2b9dd8ce9
Furover <133924982+Furover@users.noreply.github.com>
2024-02-06 23:43:49 -0300

    It was actually mvc lol

---
+++ C#/ASP-NET-CORE_MVC-test/ASP-NET-CORE_MVC-test/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using ASP_NET_CORE_MVC_test.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_NET_CORE_MVC_test.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+            ViewData["SubMessage"] = "C# is not my passion";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
